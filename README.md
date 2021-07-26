a4d538bb8aeda25e929cf5c0b3f5a513404d0bc0
Sarah B <sarahbeavan@gmail.com>
2021-07-27 11:53:24 +1200

    Filled in other models.general Moved images to specific subfolders

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 A webapp created for Massey University's 289.212 Web and Interactive Production course.
 
-A collaborative recipe-sharing website, for people to share  their recipes, and find recipes others have shared. With commenting and upvoting for feedback and additions.
+A collaborative recipe-sharing website, for people to share  their recipes, and find recipes others have shared. With commenting and upvoting for feedback and additions (Stretch Goal).
 
 ## Resources
 - https://flask.palletsprojects.com/en/2.0.x/
--- recipebox/models/general.py
+++ recipebox/models/general.py
@@ -14,12 +14,13 @@ class Ingredient(db.Model):
     """
     __tablename__ = "ingredient"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30))
-    picture  = db.Column(db.String(150))
+    name = db.Column(db.String(30), nullable=False)
+    picture  = db.Column(db.String(150), nullable=False)
+    recipes = db.relationship('RecipeIngredient', backref="ingredient", lazy=True)
 
     def __init__(self, name):
         self.name = name
-        self.picture = os.path.join(current_app.config['STATIC_DIR'], name + ".png")
+        self.picture = os.path.join(*[current_app.config['STATIC_DIR'], "ingredients", name + ".png"])
 
 
 class Recipe(db.Model):
@@ -29,10 +30,17 @@ class Recipe(db.Model):
     Attributes:
         name (str): name of recipe
         picture (str): path to picture
-        ingredients (RecipeItem):
+        ingredients (RecipeIngredient): many to one relationship with RecipeIngredients
     """
     __tablename__ = "recipe"
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    picture = db.Column(db.String(150), nullable=False)
+    ingredients = db.relationship('RecipeIngredient', backref="recipe", lazy=True)
+
+    def __init__(self, name):
+        self.name = name
+        self.picture = os.path.join(*[current_app.config['STATIC_DIR'], "recipes", name + ".png"])
 
 class RecipeIngredient(db.Model):
     """
@@ -41,9 +49,20 @@ class RecipeIngredient(db.Model):
     A specific ingredient entry in a recipe.
 
     Attributes:
+        recipe_id: recipe this belongs to
         ingredient (Ingredient): ingredient to reference
         quantity (int): amount of the ingredient
         unit (str): unit of measurement
     """
     __tablename__ = "recipe_ingredient"
     id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    unit = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, recipe, ingredient, quantity, unit):
+        self.recipe_id = recipe
+        self.ingredient_id = ingredient
+        self.quantity = quantity
+        self.unit = unit
